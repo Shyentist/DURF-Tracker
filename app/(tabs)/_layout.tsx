2943c42fe28a8efcaedeeb921e95f3e297a539ce
Shyentist <pasqualebuonomo@hotmail.it>
2024-12-10 18:18:08 +0100

    stylized Tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,22 +1,43 @@
 import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{
-        headerShown: false
-      }} >
-      <Tabs.Screen name="index" 
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#000000',
+        tabBarInactiveTintColor: '#000000',
+        tabBarStyle: { backgroundColor: '#FFD700' },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
         options={{
-          title: 'Characters'
-        }} />
-      <Tabs.Screen name="manuals" 
+          title: 'Characters',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="people" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="manuals"
         options={{
-          title: 'Manuals'
-        }} />
-      <Tabs.Screen name="settings"  
+          title: 'Manuals',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="book" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
         options={{
-          title: 'Settings'
-        }}/>
+          title: 'Settings',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
